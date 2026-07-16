cbf4331814c054a7f8e5c0c544c4ecb87a0e4ff0
ArunKumarS023 <arunaruns3424@gmail.com>
2026-07-16 19:36:28 +0530

    Add files via upload

---
+++ resume_screener/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # 🔐 AUTH (VERY IMPORTANT)
+    path('login/', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
+    # APP
+    path('', include('app.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
